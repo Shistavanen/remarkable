707ae7557c499591f056723acec46b0be8185482
Nick Hays <nicholasahays@gmail.com>
2023-03-08 11:06:40 -0700

    Initial tag model

---
+++ remarkable/src/models/Tag.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+
+interface ITag {
+  name: string;
+  bookmarks: mongoose.Types.ObjectId[];
+}
+
+const tagSchema = new Schema<ITag>({
+  name: {type: String, required: true, unique: true},
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bookmark', default: [] }]
+})
+
+const Tag = mongoose.model('Tag', tagSchema);
+
+export default Tag;
