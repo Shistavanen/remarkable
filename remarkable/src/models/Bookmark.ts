74940a7fedfe24b84f59e1e5ad321aa6a7f7a825
Nick Hays <nicholasahays@gmail.com>
2023-03-08 10:51:52 -0700

    Initial bookmark schema and model

---
+++ remarkable/src/models/Bookmark.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+interface IBookmark {
+  url: string,
+  title: string,
+  tags: Array<{ type: mongoose.Types.ObjectId, ref: 'Tag' }>;
+}
+
+const bookmarkSchema = new Schema<IBookmark>({
+  url: { type: String, required: true },
+  title: { type: String, requred: true },
+  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }]
+})
+
+const Bookmark = mongoose.model('Bookmark', bookmarkSchema);
+
+export default Bookmark;
