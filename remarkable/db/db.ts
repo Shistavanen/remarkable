4d75046f405e0d343c9bafd9658a6c2f67b354d5
Nick Hays <nicholasahays@gmail.com>
2023-03-08 11:19:15 -0700

    Initial db connecting function

---
+++ remarkable/db/db.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+import mongoose, { ConnectOptions } from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    } as ConnectOptions);
+    console.log('Successfully connected to db');
+  } catch(err) {
+    console.error('MongoDB connection failed:', err);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
