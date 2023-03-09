603995b74ab543301502264b03c245a5dd4cf49e
Nick Hays <nicholasahays@gmail.com>
2023-03-09 10:58:40 -0700

    Adds basic search box that returns bookmarks based on tags

---
+++ remarkable/src/components/SearchBar.tsx
@@ -0,0 +1,55 @@
+import { Formik, Form, Field } from 'formik';
+import {useState} from 'react';
+import axios from 'axios';
+import * as Yup from 'yup';
+
+const SearchSchema = Yup.object().shape({
+  tags: Yup.string().required('Tags are required'),
+});
+
+export default function SearchBar() {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    console.log('TAGS: ', values.tags)
+    try {
+      const response = await axios(`/api/getBookmarks?tags=${values.tags}`);
+      const data = await response.data;
+      setBookmarks(data);
+      setSubmitting(false);
+    } catch (error) {
+      console.error(error);
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Search Bookmarks</h2>
+      <Formik
+        initialValues={{ tags: '' }}
+        validationSchema={SearchSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="tags" placeholder="Enter comma-separated tags" />
+            <button type="submit" disabled={isSubmitting}>Search</button>
+          </Form>
+        )}
+      </Formik>
+      {bookmarks.length > 0 && (
+        <div>
+          <h2>Bookmarks</h2>
+          <ul>
+            {bookmarks.map(bookmark => (
+              <li key={bookmark._id}>
+                <a href={bookmark.url}>{bookmark.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
