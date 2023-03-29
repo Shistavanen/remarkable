8db547fda0441e034a8d25993e420434c60e2e88
Nick Hays <nicholasahays@gmail.com>
2023-03-28 21:21:11 -0700

    Removes unnecessary usestate

--- remarkable/src/components/InputContainer.tsx
+++ remarkable/src/components/InputContainer.tsx
@@ -1,11 +1,9 @@
-import {useState, useEffect } from 'react';
+import 'react-tabs/style/react-tabs.css'; //Default styling for react-tabs
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import SearchBar from './SearchBar';
 import BookmarkForm from './BookmarkForm';
-import 'react-tabs/style/react-tabs.css'; //Default styling for react-tabs
 
 export default function InputContainer() {
-  const [isSearchBar, setIsSearchBar] = useState(true);
 
   return (
     <Tabs>
