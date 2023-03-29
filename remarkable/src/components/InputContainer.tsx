324478cc978714a1918d97200b399fa926e1ad8c
Nick Hays <nicholasahays@gmail.com>
2023-03-28 21:19:17 -0700

    Adds default styling for react tabs

--- remarkable/src/components/InputContainer.tsx
+++ remarkable/src/components/InputContainer.tsx
@@ -2,7 +2,7 @@ import {useState, useEffect } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import SearchBar from './SearchBar';
 import BookmarkForm from './BookmarkForm';
-import 'react-tabs/style/react-tabs.css';
+import 'react-tabs/style/react-tabs.css'; //Default styling for react-tabs
 
 export default function InputContainer() {
   const [isSearchBar, setIsSearchBar] = useState(true);
